30e24b0b317464546b283177760da8a42086dde2
ethanchung936 <ethanchung936@gmail.com>
2023-11-28 00:01:55 -0700

    added mcts to choose the move

--- assignment4/RENAME_your_directory_name/Ninuki.py
+++ assignment4/RENAME_your_directory_name/Ninuki.py
@@ -10,8 +10,10 @@ from board_base import DEFAULT_SIZE, GO_POINT, GO_COLOR
 from board import GoBoard
 from board_util import GoBoardUtil
 from engine import GoEngine
+from mcts import MCTS, TreeNode
 import time
 import random
+import numpy as np
 from board_base import (
     BLACK,
     WHITE,
@@ -32,12 +34,22 @@ class A4SubmissionPlayer(GoEngine):
         """
         GoEngine.__init__(self, "Go0", 1.0)
         self.time_limit = 1
+        self.MCTS = MCTS()
+        
+    def update(self, move: GO_POINT) -> None:
+        self.parent = self.MCTS.root
+        self.MCTS.update_with_move(move)
 
     def get_move(self, board: GoBoard, color: GO_COLOR) -> GO_POINT:
         """
         Implement for assignment 4
         """
-        pass
+        point = self.MCTS.get_move(board, color, 1, True, 50, np.sqrt(2), True)
+        coord = point_to_coord(point, board.size)
+        move = format_point(coord)
+        # self.MCTS.print_pi(board)
+        self.update(move)
+        return move
 
     def set_time_limit(self, time_limit):
         self.time_limit = time_limit
